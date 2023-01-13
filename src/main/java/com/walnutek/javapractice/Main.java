cb2b0e3763e63eadbc64e062aa5fb23505bf0a0d
Xunhaoz <leo20020529@gmail.com>
2023-01-13 11:44:34 +0800

    V3

--- src/main/java/com/walnutek/javapractice/Main.java
+++ src/main/java/com/walnutek/javapractice/Main.java
@@ -1,7 +1,9 @@
 package com.walnutek.javapractice;
 
 import com.walnutek.javapractice.Monetary;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -11,12 +13,20 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class Main {
+    static final String[] targetCurrencyList = {"USD", "GBP", "CNY", "EUR"};
+
     public static void main(String[] args) throws IOException {
         String getUrl = "https://rate.bot.com.tw/xrt?Lang=zh-TW";
-        Document doc = Jsoup.parse(new URL(getUrl).openStream(), "UTF-8", getUrl);
+        Connection.Response response = Jsoup.connect(getUrl)
+                .ignoreContentType(true)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+                .timeout(12000)
+                .followRedirects(true)
+                .execute();
 
-        String[] querys = {"美金 (USD)", "英鎊 (GBP)", "人民幣 (CNY)", "歐元 (EUR)"};
-        ArrayList<Monetary> monetarys = new ArrayList<Monetary>();
+        Document doc = response.parse();
+
+        ArrayList<Monetary> monetaries = new ArrayList<Monetary>();
 
         Element table = doc.select("table").get(0);
         Elements rows = table.select("tr");
@@ -24,33 +34,29 @@ public class Main {
             Elements cols = row.select("td");
 
             String currency = "";
-            boolean selected = false;
 
             if (cols.size() > 0) {
                 currency = cols.get(0).getElementsByClass("hidden-phone print_show").html();
             }
 
-            for (String query : querys) {
-                if (query.equals(currency)) {
-                    selected = true;
-                    monetarys.add(new Monetary(currency));
+            for (String query : targetCurrencyList) {
+                if (currency.contains(query)) {
+                    Monetary tmp = new Monetary(currency);
+                    tmp.setCashRateIn(cols.get(1).html());
+                    tmp.setCashRateOut(cols.get(2).html());
+                    tmp.setSpotRateIn(cols.get(3).html());
+                    tmp.setSpotRateOut(cols.get(4).html());
+                    monetaries.add(tmp);
                 }
             }
-
-            if (selected) {
-                monetarys.get(monetarys.size() - 1).setCash_rate_in(Float.parseFloat(cols.get(1).html()));
-                monetarys.get(monetarys.size() - 1).setCash_rate_out(Float.parseFloat(cols.get(2).html()));
-                monetarys.get(monetarys.size() - 1).setSpot_rate_in(Float.parseFloat(cols.get(3).html()));
-                monetarys.get(monetarys.size() - 1).setSpot_rate_out(Float.parseFloat(cols.get(4).html()));
-            }
         }
-        for (Monetary monetary : monetarys) {
+
+        for (Monetary monetary : monetaries) {
             System.out.println(monetary.getCurrency());
-            System.out.println(monetary.getCash_rate_in());
-            System.out.println(monetary.getCash_rate_out());
-            System.out.println(monetary.getSpot_rate_in());
-            System.out.println(monetary.getSpot_rate_out());
+            System.out.println(monetary.getCashRateIn());
+            System.out.println(monetary.getCashRateOut());
+            System.out.println(monetary.getSpotRateIn());
+            System.out.println(monetary.getSpotRateOut());
         }
-
     }
 }
--- src/main/java/com/walnutek/javapractice/Monetary.java
+++ src/main/java/com/walnutek/javapractice/Monetary.java
@@ -2,48 +2,56 @@ package com.walnutek.javapractice;
 
 public class Monetary {
     private String currency;
-    private Float cash_rate_in = null;
-    private Float cash_rate_out = null;
-    private Float spot_rate_in = null;
-    private Float spot_rate_out = null;
+    private Float cashRateIn = null;
+    private Float cashRateOut = null;
+    private Float spotRateIn = null;
+    private Float spotRateOut = null;
 
-    public Monetary(String currency){
+    private float parseFloat(String s) {
+        try {
+            return Float.parseFloat(s);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public Monetary(String currency) {
         this.currency = currency;
     }
 
-    public void setCash_rate_in(Float cash_rate_in) {
-        this.cash_rate_in = cash_rate_in;
+    public void setCashRateIn(String cashRateIn) {
+        this.cashRateIn = parseFloat(cashRateIn);
     }
 
-    public void setCash_rate_out(Float cash_rate_out) {
-        this.cash_rate_out = cash_rate_out;
+    public void setCashRateOut(String cashRateOut) {
+        this.cashRateOut = parseFloat(cashRateOut);
     }
 
-    public void setSpot_rate_in(Float spot_rate_in) {
-        this.spot_rate_in = spot_rate_in;
+    public void setSpotRateIn(String spotRateIn) {
+        this.spotRateIn = parseFloat(spotRateIn);
     }
 
-    public void setSpot_rate_out(Float spot_rate_out) {
-        this.spot_rate_out = spot_rate_out;
+    public void setSpotRateOut(String spotRateOut) {
+        this.spotRateOut = parseFloat(spotRateOut);
     }
 
     public String getCurrency() {
         return currency;
     }
 
-    public Float getCash_rate_in() {
-        return cash_rate_in;
+    public Float getCashRateIn() {
+        return cashRateIn;
     }
 
-    public Float getCash_rate_out() {
-        return cash_rate_out;
+    public Float getCashRateOut() {
+        return cashRateOut;
     }
 
-    public Float getSpot_rate_in() {
-        return spot_rate_in;
+    public Float getSpotRateIn() {
+        return spotRateIn;
     }
 
-    public Float getSpot_rate_out() {
-        return spot_rate_out;
+    public Float getSpotRateOut() {
+        return spotRateOut;
     }
 }
